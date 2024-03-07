a388445907d7850e21269cb2827adddd9e02eae4
aj1565 <aj1565@host.docker.internal>
2024-03-07 10:54:02 +0000

    Updated basic stuff

--- libGDX/core/src/com/heslington_hustle/screens/minigames/TestMinigame.java
+++ libGDX/core/src/com/heslington_hustle/screens/minigames/TestMinigame.java
@@ -1,5 +1,22 @@
 package com.heslington_hustle.screens.minigames;
 
-public class TestMinigame {
+import com.badlogic.gdx.Screen;
+import com.heslington_hustle.game.HeslingtonHustle;
+import com.heslington_hustle.screens.MinigameScreen;
+
+public class TestMinigame extends MinigameScreen implements Screen {
+
+	public TestMinigame(HeslingtonHustle game, float difficultyScalar) {
+		super(game, difficultyScalar);
+		// TODO Auto-generated constructor stub
+	}
+	
+	public void render(float delta) {
+		
+	}
+	
+	public void dispose() {
+		
+	}
 
 }
